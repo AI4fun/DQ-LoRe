b5dd43cf5ae7b36edd3de8bae3b230253394616f
UGUESS-lzx <63826387+UGUESS-lzx@users.noreply.github.com>
2024-02-28 16:26:58 +0800

    Update strategyqa.py

--- src/dataset_readers/dataset_wrappers/strategyqa.py
+++ src/dataset_readers/dataset_wrappers/strategyqa.py
@@ -10,7 +10,7 @@ def add_numbering(text):
     numbered_text = ''
 
     for i, sentence in enumerate(sentences, 1):
-        numbered_text += f'{i}. {sentence}\n'  # ��ÿ������ǰ�������
+        numbered_text += f'{i}. {sentence}\n' 
 
     return numbered_text
 
@@ -36,28 +36,25 @@ def get_qa(entry):
 
 @field_getter.add("gen_a")
 def get_gen_a(entry):
-    #return "{ice_prompt}{question}, You need to output a json format where the thought tag is the reasoning process and the result tag is the final result, which is represented only by numbers\t".format(ice_prompt="{ice_prompt}", question=get_q(entry))
     return "{ice_prompt}{question}\t".format(ice_prompt="{ice_prompt}", question=get_q(entry))
 
 @field_getter.add("complex_qa")
 def get_complex_qa(entry):
     ans = get_a(entry)
-    #ans = remove_brackets_content(ans)
+    ans = remove_brackets_content(ans)
     pattern = r"####\s*(-?\d+)"
     replacement = r"The answer is \1"
     new_ans = re.sub(pattern, replacement, ans)
     return "Question:{question}\nA: Let's think step by step.\n{answer}".format(question = get_q(entry), answer = new_ans)
-    #return "Question:{question}\n Answer:{answer}".format(question = get_q(entry), answer = get_a(entry))
     
 @field_getter.add("number_qa")
 def get_number_qa(entry):
     ans = get_a(entry)
-    #ans = remove_brackets_content(ans)
+    ans = remove_brackets_content(ans)
     pattern = r"####\s*(-?\d+)"
     replacement = r"The answer is \1"
     new_ans = re.sub(pattern, replacement, ans)
     return "Question:{question}\nA: Let's think step by step.\n{answer}".format(question = get_q(entry), answer = add_numbering(new_ans))
-    #return "Question:{question}\n Answer:{answer}".format(question = get_q(entry), answer = get_a(entry))
 
 class DatasetWrapper(ABC):
     name = "strategyqa"
